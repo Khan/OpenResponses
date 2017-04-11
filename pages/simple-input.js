56e20b0564ee4e4303f8dc3cb9b6f26fa0e2c8c1
Andy Matuschak <andy@khanacademy.org>
2017-04-11 15:45:24 -0700

    Added a little paragraph module

--- pages/simple-input.js
+++ pages/simple-input.js
@@ -1,10 +1,14 @@
 import Heading from "../lib/components/heading.js";
+import Paragraph from "../lib/components/paragraph.js";
 import RichEditor from "../lib/components/rich-editor.js";
 import sharedStyles from "../lib/styles.js";
 
 export default () => (
   <div>
     <Heading text="Testing 1, 2, 3!" />
+    <Paragraph text="What what" />
+    <RichEditor />
+    <RichEditor />
     <RichEditor />
   </div>
 );
