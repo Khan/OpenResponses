5bbfdbc95b1480e1a655e9b01aa5f95dab5d5f22
Andy Matuschak <andy@khanacademy.org>
2017-04-11 11:56:31 -0700

    Configuring Aphrodite

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import { StyleSheet, css } from "aphrodite";
+
+import sharedStyles from "../lib/styles.js";
+
 export default () => {
-  return <h1>Hello, world.</h1>;
+  return <h1 className={css(styles.header)}>Hello, world.</h1>;
 };
+
+const styles = StyleSheet.create({
+  header: {
+    ...sharedStyles.typography.subjectHeadingDesktop
+  }
+});
