aa4ba547c5fca0164eddffe04f334fbb978dc750
Andy Matuschak <andy@khanacademy.org>
2017-04-11 15:29:33 -0700

    Added simple heading

--- pages/simple-input.js
+++ pages/simple-input.js
@@ -1,4 +1,10 @@
+import Heading from "../lib/components/heading.js";
 import RichEditor from "../lib/components/rich-editor.js";
 import sharedStyles from "../lib/styles.js";
 
-export default () => <RichEditor />;
+export default () => (
+  <div>
+    <Heading text="Testing 1, 2, 3!" />
+    <RichEditor />
+  </div>
+);
