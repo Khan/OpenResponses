85afa20f3bafc63d954e939da5b85f8da37e2d55
Andy Matuschak <andy@khanacademy.org>
2017-04-11 18:01:13 -0700

    Added multiple choice component

--- pages/simple-input.js
+++ pages/simple-input.js
@@ -1,4 +1,5 @@
 import Heading from "../lib/components/heading.js";
+import MultipleChoice from "../lib/components/multiple-choice.js";
 import Paragraph from "../lib/components/paragraph.js";
 import RichEditor from "../lib/components/rich-editor.js";
 import sharedStyles from "../lib/styles.js";
@@ -6,7 +7,14 @@ import sharedStyles from "../lib/styles.js";
 export default () => (
   <div>
     <Heading text="Testing 1, 2, 3!" />
-    <Paragraph text="What _what_ $x + 3 = 10$" />
+    <Paragraph text="What _what_ $x^2 + 30 = 100$" />
+    <MultipleChoice
+      choices={[
+        "This is a choice",
+        "And this is another choice",
+        "But this is actually a bee"
+      ]}
+    />
     <RichEditor />
   </div>
 );
