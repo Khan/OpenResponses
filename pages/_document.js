56be39bb03c1d6f025ed01a92a128527ae049ad4
Andy Matuschak <andy@khanacademy.org>
2017-04-24 10:33:42 -0700

    Fixed scratchpad in Safari

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,6 @@
 import Document, { Head, Main, NextScript } from "next/document";
 import { StyleSheetServer } from "aphrodite";
 
-if (
-  !!(typeof window !== "undefined" &&
-    window.document &&
-    window.document.createElement)
-) {
-  require("pepjs");
-}
-
 export default class AphroditeDocument extends Document {
   static async getInitialProps({ renderPage }) {
     const { html, css } = StyleSheetServer.renderStatic(() => renderPage());
@@ -24,6 +16,7 @@ export default class AphroditeDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css"
           />
+          <script src="https://code.jquery.com/pep/0.4.2/pep.js" />
           <style dangerouslySetInnerHTML={{ __html: this.props.css.content }} />
           <style>
             {
