b1e1b0a089e53979d7bf07c0dd3fef1e91b99909
Andy Matuschak <andy@khanacademy.org>
2017-05-02 21:12:29 -0700

    Using a more now-compatible env var setup

---
+++ env-config.js
@@ -0,0 +1,12 @@
+require("dotenv").config({ path: "env.production" });
+
+module.exports = {
+  firebaseConfiguration: {
+    apiKey: process.env["FIREBASE_API_KEY"],
+    authDomain: process.env["AUTH_DOMAIN"],
+    databaseURL: process.env["DATABASE_URL"],
+    projectId: process.env["PROJECT_ID"],
+    storageBucket: process.env["STORAGE_BUCKET"],
+    messagingSenderId: process.env["MESSAGING_SENDER_ID"],
+  },
+};
