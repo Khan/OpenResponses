6050d73aeacb43b22500d546a1d035fbfb2399a0
Andy Matuschak <andy@khanacademy.org>
2018-01-16 17:37:18 -0800

    Getting a few initial pieces in place for new lighter prototype structure

---
+++ pages/neue.js
@@ -0,0 +1,39 @@
+// @flow
+import React from "react";
+import Router from "next/router";
+
+import { signIn } from "../lib/auth";
+import PageContainer from "../lib/components/neue/page-container";
+import Prompt from "../lib/components/neue/prompt";
+
+type State = {
+  ready: boolean,
+};
+
+type Props = {};
+
+export default class NeueFlowPage extends React.Component {
+  state: State;
+  props: Props;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      ready: false,
+    };
+  }
+
+  render = () => {
+    return (
+      <PageContainer>
+        <Prompt
+          title="Testing testing 1 2 3"
+          prompt={`Foo bar baz bat baz quux bar baz _Foo bar baz bat baz quux bar_ baz Foo bar baz bat baz quux bar baz Foo bar baz bat baz quux bar baz Foo bar baz bat baz quux bar baz
+          
+bat baz quux bar baz bat baz quux`}
+        />
+      </PageContainer>
+    );
+  };
+}
