621c75b1d8db75cc0a2b2ac849216a59584b26fb
Andy Matuschak <andy@khanacademy.org>
2018-01-29 09:18:54 -0800

    Updated activity prompt

--- pages/neue.js
+++ pages/neue.js
@@ -579,14 +579,18 @@ export default class NeueFlowPage extends React.Component {
         <PageContainer>
           <Prompt
             title={title}
-            prompt={`What is the message of this cartoon? Would you say the artist supported or opposed equal rights for freedmen?
-            
-The text under the cartoon reads: "Franchise. And not this man?"`}
+            prompt={`Analyze the cartoon, and answer these questions:
+
+1. What was the message of this cartoon?
+2. Would you say the artist supported or opposed equal rights for freedmen?`}
             stimuli={[
               {
                 imageURL: "http://andymatuschak.org/Franchise.jpg",
               },
             ]}
+            postStimuliPrompt={`Caption: FRANCHISE. AND NOT THIS MAN?
+            
+Source: Thomas Nast was a political cartoonist who drew for a New York magazine called Harper’s Weekly. He supported the North’s side during the Civil War. This cartoon was published in 1865.`}
           />
           <p />
           <div ref={ref => (this.cardWorkspaceContainerRef = ref)}>
